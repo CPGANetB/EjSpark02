86f2ff73671dc0e994080a52b4a724501d78dd69
GPGANetB <cpganet@gmail.com>
2020-01-05 13:17:56 -0300

    armado de html en CreaHtml

--- src/main/java/carrera/CreaHtml.java
+++ src/main/java/carrera/CreaHtml.java
@@ -2,18 +2,76 @@ package carrera;
 import java.util.*;
 
 public class CreaHtml{
-    private String html;
+    //private String html;
     private Participante unParti;
     private ArrayList<Participante> aLst;
 
-    public StringBuffer CreaHtml(String html, Participante unParti, ArrayList aLst){
-
-
+    public static StringBuffer CreaHtml(String html, Participante unParti, ArrayList aLst){
+        //String html = html;
+        Participante parti = unParti;
         StringBuffer buffer = new StringBuffer();
         buffer.append("<!DOCTYPE html><html lang=\"es\">\n");
         buffer.append("<head><meta charset=\"UTF-8\">\n");
+        buffer.append("html = " + html + "\n");
+        if (html == "create") {
+            buffer.append("<title> Crear Participante </title>\n");
+            buffer.append("</head>\n");
+            buffer.append("<body><div><div id=\"quever\"><h2> Alta de participante </h2><h3> Datos a ingresar: </h3><table>\n");
+            buffer.append("<form method=\"post\" action=\"/participants/create\" >\n");
+            buffer.append("<tr><th>Nombre: </th><th><input id=\"datoing1\" name=\"noming1\" type=\"text/html\" ></th></tr>\n");
+            buffer.append("<tr><th>Score: </th><td><input id=\"datoing2\" name=\"scoing2\" type=\"text\" value=\"123\"></td></tr>\n");
+            buffer.append("<tr><td colspan=\"2\" ><br><input type=\"submit\" name=\"confi3\"value=\"Ingresa o 0 para salir\"></td></tr>\n");
+            buffer.append("<tr><td></td></tr></form></table>\n");
+            buffer.append("</table></div><br>\n");
+            buffer.append("<div ><table ><tr><th id=\"score\"></th><th id=\"nombre\"></th></tr>\n");
+            buffer.append("<tr><td align=\"center\"></th><td colspan=\"2\" align=\"left\"></td><div id=\"respuesta\"></div></tr></table></div>\n");
+        }
 
+        if (html == "newparticipant") {
+            buffer.append("<title> Nuevo participante </title>\n");
+            buffer.append("</head>\n");
+            buffer.append("<body><div><div id=\"quever\"><h2> Nuevo participante </h2>");
+            if (parti.nombre != "") {
+                buffer.append("<h3> Se agregó: </h3><table >\n");
+                buffer.append("<tr><th>Nombre: </th><th><div id=\"newNom\"><strong>" + parti.nombre  + " </strong></div></th></tr>\n");
+                buffer.append("<tr><th>Score: </th><td><div id=\"newSco\"><strong>" + parti.score + " </strong></div></td></tr>\n");
+                buffer.append("<tr><td colspan=\"2\" align=\"center\"><br></td></tr></table>\n");
+            }
+        }
 
+        if (html == "participants") {
+            ArrayList aLis = new ArrayList(aLst);
+            buffer.append("<title> Participantes </title>\n");
+            buffer.append("</head>\n");
+            buffer.append("<body><div><div id=\"quever\"><h2> Listado de Participantes </h2><h3><i>Lista ordenada:</i></h3><a href=\"http://localhost:8888/participants/create\">Agrega nuevo</a><br>\n");
+            if (aLis.toString() != "") {
+                buffer.append("<br><table ><tr bgcolor=\"lightblue\"><th><u>Nombre</u></th><th><th><u>Score</u></th></th></tr>\n");
+                buffer.append("<tr id=\"ordLst\">");
+                Collections.sort(aLis, new Comparator<Participante>() {
+                    @Override
+                    public int compare(Participante p1, Participante p2) {
+                        // Aqui esta el truco, ahora comparamos p2 con p1 y no al reves como antes
+                        return new Double(p2.getScore()).compareTo(new Double(p1.getScore()));
+                    }
+                });System.out.println("340 =>>>> sort arLis.add(participan)");
+                Participante parti1 = new Participante();
+                Iterator iter = aLst.iterator();
+                while (iter.hasNext()) {
+                    parti1 = (Participante) iter.next();System.out.println("344 =>>>> iter arLis.add(participan)" + parti1);
+                    buffer.append("<tr><td><b>" + parti1.nombre + "</b></td><td><td><b>" + parti1.score + "</b></td></td></tr></tr>\n");
+                    //buffer.append("<tr><td><b>" +  + "</b></td><td><td><b>" + parti.score + "</b></td></td></tr></tr>\n");
+                }
+                //Participante parti = new Participante();
+                Iterator iteParti = aLis.iterator();
+                while (iteParti.hasNext()) {
+                    parti = (Participante) iteParti.next();System.out.println("344 =>>>> iter arLis.add(participan)" + parti);
+                    buffer.append("<tr><td><b>" + parti.nombre + "</b></td><td><td><b>" + parti.score + "</b></td></td></tr></tr>\n");
+                    //buffer.append("<tr><td><b>" +  + "</b></td><td><td><b>" + parti.score + "</b></td></td></tr></tr>\n");
+                }
+            }
+            buffer.append("<tr><td colspan=\"2\" align=\"center\"><br></td></tr></table>\n");
+        }
+        buffer.append("</div></body></html>\n");
         StringBuffer bufferc = new StringBuffer(buffer.toString());
         return bufferc;
     }
--- src/main/java/carrera/Principal.java
+++ src/main/java/carrera/Principal.java
@@ -1,4 +1,7 @@
 package carrera;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -8,12 +11,18 @@ import static spark.Spark.*;
 public class Principal {
     private static ArrayList<Participante> participantes = new ArrayList();
     static Scanner scn = new Scanner(System.in);
-    Participante participante = new Participante();
-    StringBuffer nvaPag = new CreaHtml().CreaHtml("pag", participante, participantes );
 
     public static void main(String[] args) {
         port(8888);
         String opc = "";
+        Participante participante = new Participante();
+        StringProperty envio = new SimpleStringProperty();
+        Participante nvop= new Participante();
+        ArrayList nvoa= new ArrayList();
+        envio.set(CreaHtml.CreaHtml("participants",nvop,nvoa).toString());
+        get("/participants/", (req, res) -> envio.get());
+        awaitInitialization();
+        StringBuffer nvaPag = new CreaHtml().CreaHtml("pag", participante, participantes );
         participantes.add(new Participante("PEPE-01", 46.0));
         participantes.add(new Participante("JUAN-02", 15.0));
         participantes.add(new Participante("PIPO-03", 89.0));
@@ -34,7 +43,9 @@ public class Principal {
         } while (!opc.equals("0"));
         //System.out.println("Ordena");
         Ordena();
-        //System.out.println("Termina");
+        nvaPag = CreaHtml.CreaHtml("participants", participante, participantes);
+        nvaPag.toString();
+        System.out.println("Termina" + nvaPag.toString());
     }
 
     public static void Ordena(){
